7cb807323c6ebfa9d716b84dea65791445868e91
LeaClover <plesen_14@mail.ru>
2022-07-15 22:04:02 +0300

    editting by task2 file methods

--- Lesson6/HomeWork/Methods.cs
+++ Lesson6/HomeWork/Methods.cs
@@ -5,6 +5,15 @@ namespace GBhomework6
     class Methods
     {
         public string? str;
+
+        public double k1;
+        public double k2;
+        public double b1;
+        public double b2;
+
+        public double x;
+        public double y;
+        
         public int [] GetArrayFromString(string str)
         {
             int [] arrayS = Array.ConvertAll(str.Split(' '), int.Parse);
@@ -20,6 +29,16 @@ namespace GBhomework6
             return array;
         }
 
+        public double [] PrintArrayD (double [] array)
+        {
+            for(int i = 0; i<array.Length; i++)
+            {
+                Console.Write(array[i] + " | ");
+            }
+            return array;
+        }
+
+
         public int GetCount (int [] array)
         {
             int count = 0;
@@ -31,6 +50,17 @@ namespace GBhomework6
                 }
             }
             return count;
+        }
+
+        public double [] GetCommonPoint(double k1, double b1, double k2, double b2)
+        {
+            double [] array = new double [2];
+            x = (b2 - b1)/(k1 - k2);
+            y = k1*x + b1;
+            array[0] = x;
+            array[1] = y;
+            return array;
+
         }
     }
 }
