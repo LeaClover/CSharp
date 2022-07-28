4a52b05ceac0547959413ea98003656eb87986bb
LeaClover <plesen_14@mail.ru>
2022-07-28 14:29:52 +0300

    add new project Hw - file program

---
+++ Lesson9/HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿using GBhomework9;
+
+namespace GBhomework9
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Text main = new();
+            Structure choice = new();
+            main.maintext();
+            choice.choice = Convert.ToInt32(Console.ReadLine());
+            choice.GetChoice(choice.choice);
+        }
+    }
+}
